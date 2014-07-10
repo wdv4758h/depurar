c662046e7c831b4d9a64d4af9181021615e1367a
dv <wdv4758h@gmail.com>
2014-07-10 19:26:55 +0800

    add exception support

--- depurar.py
+++ depurar.py
@@ -19,14 +19,24 @@ def printc(func):
         paramaters += skwarg
 
         print('{}{}({})'.format(printc.indent * printc.depth, func.__name__, paramaters))
+
+        depth = printc.depth
         printc.depth += 1
-        ret = func(*args, **kwargs)
+        try:
+            ret = func(*args, **kwargs)
+        except Exception as e:
+            print('{}{}{}'.format(printc.indent * depth, printc.error, e))
+            printc.depth = depth
+            raise e
         printc.depth -= 1
+
         if ret != None:
             print('{}{}({}) => {}'.format(printc.indent * printc.depth, func.__name__, paramaters, repr(ret)))
+
         return ret
 
     return wrapper
 
 printc.depth = 0
 printc.indent = '| '
+printc.error = '>> '
